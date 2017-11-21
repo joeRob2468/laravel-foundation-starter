199c2da41ad4f5888fa95eb18e0df41505d748aa
John Boyer <joeRob2468@yahoo.com>
2017-11-21 14:51:50 -0600

    implemented view layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,10 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+@extends('layouts.app')
 
-  <!-- Styles -->
-  <link rel="stylesheet" type="text/css" href="/css/app.css">
+@section('title', 'Home')
 
-  <title>Laravel</title>
-</head>
-<body>
-  <div class="row">
-    <div class="large-12 columns text-center">
-      <h1>Welcome.</h1>
-    </div>
+@section('content')
+<div class="row">
+  <div class="large-12 columns text-center">
+    <h1>Welcome.</h1>
   </div>
-
-  <!-- Scripts -->
-  <script src="/js/app.js"></script>
-</body>
-</html>
+</div>
